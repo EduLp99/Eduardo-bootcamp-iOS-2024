0fb14d9321656127a6f1c4287342ebaf3fbf37ca
EduLp99 <edulp99@gmail.com>
2024-06-27 22:57:37 -0300

    exercicios class

--- POO-Bootcamp-Eduardo/POO-Bootcamp-Eduardo/main.swift
+++ POO-Bootcamp-Eduardo/POO-Bootcamp-Eduardo/main.swift
@@ -104,3 +104,156 @@ class Celular {
 }
 
 var iphone: Celular = Celular(modelo: "Iphone 15", preco: 8000, cor: "Preto")
+
+
+//MARK: Se caso NÃO TEM VALOR INICIAL, VOCÊ PRECISA PASSAR ELE NO INIT!!!!!!
+//MARK: O mesmo se encaixa para tipagens opcionais, POIS o opcional EXISTE ALGO DE INICIO -> QUE É O NIL! Com base nisso
+//MARK: não é obrigado a passar ele no construtor. Mas caso queira, sem problemas!
+class Bolo {
+
+  var peso: Double
+  var preco: Double
+  var sabor: [String]
+  var topoDeBolo: Bool? = nil
+
+  init(peso: Double, preco: Double, sabor: [String]) {
+    self.peso = peso
+    self.preco = preco
+    self.sabor = sabor
+  }
+
+  func adicionarSabor(sabor: String) {
+    self.sabor.append(sabor)
+  }
+}
+
+var boloAniversario: Bolo = Bolo(peso: 10, preco: 200, sabor: ["Chocolate", "Baunilha"])
+var boloCasamento: Bolo = Bolo(peso: 50, preco: 2000, sabor: ["Chocolate", "Baunilha", "Maça"])
+
+// MARK: - Exercicios Classe
+
+// Exercício 1: Conta Poupança
+// Objetivo: Desenvolver uma classe chamada 'ContaPoupanca' para gerenciar os depósitos em uma conta poupança de banco.
+//
+// Descrição:
+// Propriedades:
+// - saldo (Double): O saldo atual na conta.
+// Construtor:
+// - Inicialize o saldo.
+// Métodos:
+// - depositar(valor: Double): Adiciona(+) o valor especificado ao saldo atual. (saldo = saldo + valor especificado)
+// - consultarSaldo() -> Double: Retorna o saldo atual da conta.
+
+//   Para usar este método, você deve criar uma instância da classe, chamar este método e armazenar o valor retornado em uma variável. Em seguida, você pode imprimir esse valor.
+// print("Saldo atual: R$\(saldoAtual)")
+
+class ContaPoupanca {
+    var saldo: Double
+    
+    init(saldo: Double) {
+        self.saldo = saldo
+    }
+    
+    func depositar(valor: Double) {
+        self.saldo += valor
+    }
+    
+    func consultarSaldo() -> Double {
+        return self.saldo
+    }
+}
+
+var contaEduardo = ContaPoupanca(saldo: 2000)
+contaEduardo.depositar(valor: 1000)
+
+var saldoAtual = contaEduardo.consultarSaldo()
+print("Saldo atual: R$\(saldoAtual)")
+
+
+
+
+
+// Exercício 2: Gerenciador de Filmes
+// Objetivo: Desenvolver uma classe chamada 'Filme' para gerenciar informações sobre filmes em um acervo pessoal.
+//
+// Descrição:
+// Propriedades:
+// - titulo (String): O título do filme.
+// - diretor (String): O diretor do filme.
+// - ano (Int): O ano de lançamento do filme.
+// Construtor:
+// - Inicialize titulo, diretor e ano.
+// Métodos:
+// - exibirInfo() -> String: Retorna uma string com todas as informações do filme formatadas.
+
+//   Para usar este método, crie uma instância da classe, chame o método e armazene a informação retornada em uma variável. Em seguida, imprima essa variável para visualizar os detalhes do filme.
+
+class Filme {
+    var titulo: String
+    var diretor: String
+    var ano: Int
+    
+    init(titulo: String, diretor: String, ano: Int) {
+        self.titulo = titulo
+        self.diretor = diretor
+        self.ano = ano
+    }
+    
+    func exibirInfo() -> String {
+        return "Filme \(titulo) do diretor \(diretor) lançado em \(ano)"
+    }
+}
+
+var filmeEdu: Filme = Filme(titulo: "Interestelar", diretor: "Christopher Nolan", ano: 2014)
+var detalheFilme: String = filmeEdu.exibirInfo()
+print(detalheFilme)
+
+
+
+
+
+
+// Exercício 3: Sistema de Registro de Alunos
+// Objetivo: Criar uma classe chamada 'Aluno' para armazenar dados e calcular a média de notas de alunos em uma escola.
+//
+// Descrição:
+// Propriedades:
+// - nome (String): O nome do aluno.
+// - notas (Array<Double>): Uma lista de notas do aluno.
+// Construtor:
+// - Inicialize nome e notas.
+// Métodos:
+// - calcularMedia() -> Double: Calcula e retorna a média das notas do aluno utilizando um laço de repetição para somar as notas.
+
+//   Para utilizar este método, crie uma instância da classe, invoque o método para calcular a média, armazene esse valor em uma variável e imprima a média para visualizar.
+// print("Média das notas de \(aluno.nome): \(media)")
+
+class Aluno {
+    var nome: String
+    var notas: [Double]
+    
+    init(nome: String, notas: [Double]) {
+        self.nome = nome
+        self.notas = notas
+    }
+    
+    func calcularMedia() -> Double {
+        var somatorio: Double = 0
+        
+        for nota in self.notas {
+            somatorio += nota
+        }
+        
+        let media = somatorio/Double(self.notas.count)
+        return media
+    }
+}
+
+var alunoEduardo: Aluno = Aluno(nome: "Eduardo", notas: [7.5, 8.0, 10, 7.5])
+var mediaEduardo = alunoEduardo.calcularMedia()
+
+print("Média das notas de \(alunoEduardo.nome): \(mediaEduardo)")
+
+
+
+
