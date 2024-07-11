3bf73166ee9af147e42d6228d2a57e03302b139d
EduLp99 <edulp99@gmail.com>
2024-07-11 19:12:06 -0300

    Criando primeira tela login

---
+++ PrimeiraTelaLogin/PrimeiraTelaLogin/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PrimeiraTelaLogin
+//
+//  Created by Eduardo Felipe das Neves Abido on 10/07/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
